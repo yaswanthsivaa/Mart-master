cc4a5ed0716f70fdc98b7b76cf726de9071ae23c
yaswanthsivaa <yyaswanthreddyy@gmail.com>
2025-01-20 01:00:00 +0530

    8th commit

--- Mart-master/src/components/ProductDetail.jsx
+++ Mart-master/src/components/ProductDetail.jsx
@@ -725,7 +725,7 @@ export default function ProductDetail() {
             onClick={() => handleClick("reviews")}
             className={`pb-2 ${activeTab === "reviews" ? "text-dark fw-bold border-bottom border-3" : "text-muted"
               }`}
-            style={{ cursor: "text" }}
+            style={{ cursor: "pointer" }}
           >
             Reviews({product.reviews.length})
           </span>
